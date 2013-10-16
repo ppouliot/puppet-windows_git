b510d17e2f23b71afbe2b7099951042edc5111a2
Vijay Tripathi <vijayt.124@gmail.com>
2013-10-16 15:50:35 -0700

    Correcting Git package path
    Corrected git package path to Git-1.8.0-preview20121022

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,4 +1,4 @@
 class windows_git::params {
   $url     = 'http://cloud.github.com/downloads/msysgit/git/Git-1.8.0-preview20121022.exe'
-  $package = 'Git version 1.8.0-preview201221022'
+  $package = 'Git-1.8.0-preview20121022'
 }
