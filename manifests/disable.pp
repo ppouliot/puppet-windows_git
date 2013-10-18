cd7acf25f96c2f815eea07b8605931d2287a04a6
Peter Pouliot <ppouliot@gmail.com>
2013-10-17 22:39:16 -0400

    adding uninstall functionality

---
+++ manifests/disable.pp
@@ -0,0 +1,21 @@
+# === Class: windows_git::disable
+#
+# This class removes the installed components.
+
+class windows_git::disable (
+  $package   = $::windows_git::params::package,
+) inherits windows_git::params {
+
+  package { $package:
+    ensure  => absent,
+  }
+
+  windows_path { $git_path:
+    ensure  => absent,
+  }
+
+  file { "${::temp}/${package}.exe":
+    ensure  => absent,
+  }
+
+}
